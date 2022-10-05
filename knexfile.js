5a356d7df6434ecfa30d29f3f5a10197f86271b5
Darsh <santos.m.diego.a@gmail.com>
2022-10-05 07:44:41 -0500

    funciona :c

--- knexfile.js
+++ knexfile.js
@@ -18,22 +18,22 @@
   staging: {
     client: "mysql2",
     connection: {
-      host: "localhost",
-      port: 3306,
+      host: "containers-us-west-67.railway.app",
+      port: 5801,
       user: "root",
-      password: "halo1234",
-      database: "petcodbv2",
+      password: "lfhlMhu37lvzAcIdfOYs",
+      database: "railway",
     },
   },
 
   production: {
     client: "mysql2",
     connection: {
-      host: "localhost",
-      port: 3306,
+      host: "containers-us-west-67.railway.app",
+      port: 5801,
       user: "root",
-      password: "halo1234",
-      database: "petcodbv2",
+      password: "lfhlMhu37lvzAcIdfOYs",
+      database: "railway",
     },
   },
 };
